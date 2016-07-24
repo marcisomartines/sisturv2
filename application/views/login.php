a818732e5d98ef7a720f7709c0805dfd68fa1e51
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2016-07-24 16:57:51 -0400

    DEV-002 - Tela de Login

---
+++ application/views/login.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marciso
+ * Date: 24/07/2016
+ * Time: 14:49
+ */
+include_once "menu/l_css.php"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SISTUR V2 | Entrar</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <img class="col-xs-15 col-md-15 pull-center" src="<?=base_url()?>img/logo2.png">
+    </div><!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg">Faça o login para entrar no sistema</p>
+        <?=form_open('Principal/loginValidation')?>
+            <div class="form-group has-feedback">
+                <?=form_input(array(
+                    'name'  =>  "nome",
+                    'id'    =>  "nome",
+                    'class' =>  "form-control",
+                    'placeholder' => "Login"
+                ))?>
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <?=form_password(array(
+                    'name'  =>  "password",
+                    'id'    =>  "password",
+                    'class' =>  "form-control",
+                    'placeholder' => "Senha"
+                ))?>
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="row">
+
+                <div class="col-xs-4">
+                    <?=form_button(array(
+                        'type' => 'submit',
+                        'content' => 'Entrar',
+                        'class' => 'btn btn-primary btn-block btn-flat'
+                    ))?>
+                </div><!-- /.col -->
+            </div>
+        <?=form_close()?>
+    </div><!-- /.login-box-body -->
+</div><!-- /.login-box -->
+<?php include_once "menu/l_js.php"?>
+</body>
+</html>
+
--- application/views/menu/l_css.php
+++ application/views/menu/l_css.php
@@ -6,14 +6,15 @@
  * Time: 13:35
  */
 ?>
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-<link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
-<link rel="stylesheet" href="plugins/morris/morris.css">
-<link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-<link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
-<link rel="stylesheet" href="plugins/daterangepicker/daterangepicker-bs3.css">
-<link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+<link rel="stylesheet" href="<?=base_url()?>dist/css/AdminLTE.min.css">
+<link rel="stylesheet" href="<?=base_url()?>dist/css/skins/_all-skins.min.css">
+<link rel="stylesheet" href="<?=base_url()?>plugins/iCheck/flat/blue.css">
+<link rel="stylesheet" href="<?=base_url()?>plugins/iCheck/square/blue.css">
+<link rel="stylesheet" href="<?=base_url()?>plugins/morris/morris.css">
+<link rel="stylesheet" href="<?=base_url()?>plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+<link rel="stylesheet" href="<?=base_url()?>plugins/datepicker/datepicker3.css">
+<link rel="stylesheet" href="<?=base_url()?>plugins/daterangepicker/daterangepicker-bs3.css">
+<link rel="stylesheet" href="<?=base_url()?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
