2ea51bc65e076cc0ee7477512c1897e76cb70a57
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2016-10-11 17:45:23 -0400

    libraries cars

---
+++ application/libraries/Carros.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: marciso
+ * Date: 11/10/2016
+ * Time: 17:19
+ */
+class Carros
+{
+    protected $CI;
+    
+    public function __construct()
+    {
+        $this->CI = &get_instance();
+    }
+    
+    public function listarOnibus(){
+        $this->CI->load->model('md_carro');
+        return $this->CI->md_carro->listarOnibus();
+    }
+}
