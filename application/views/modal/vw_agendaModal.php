a081d82ec0ec408844e11b76c6f3d8e591bcc7b4
marciso.gonzalez@gmail.com <marciso.gonzalez@gmail.com>
2017-03-17 23:21:49 -0300

    Agenda - modal

--- application/views/modal/vw_agendaModal.php
+++ application/views/modal/vw_agendaModal.php
@@ -5,3 +5,106 @@
  * Date: 17/03/2017
  * Time: 22:43
  */
+?>
+<div class="modal fade" id="cadastrarAgenda" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel"><b>Cadastrar Viagem</b></h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <input type="hidden" id="tipo_cadastro" value="0" />
+                    <input type="hidden" id="acao_cadastro" value="1" />
+                    <input type="hidden" id="id_tour" name="id_tour" />
+                    <div class="col-md-12">
+                        <?php
+                        echo "<div class='row'>";
+                        echo "<div class='col-md-4'>";
+                        echo validation_errors();
+                        $this->db->where('tipo_cliente',1);
+                        $query = $this->db->get('tb_clients');
+                        $cliente[] = '';
+                        foreach ($query->result() as $clt) {
+                            $cliente[$clt->id_clients] = $clt->nome;
+                        }
+                        echo form_label('Cliente: ');
+                        echo form_dropdown('id_client', $cliente, 'v', 'class=form-control');
+                        echo '</div>';
+                        echo "<div class='col-md-2'>";
+                        $this->db->where('status', 'A');
+                        $query = $this->db->get('tb_cars');
+                        $opcao[] = '';
+                        foreach ($query->result() as $bus) {
+                            $opcao[$bus->id_cars] = $bus->codigo . ' - ' . $bus->modelo;
+                        }
+                        echo form_label('Ônibus: ');
+                        echo form_dropdown('id_car', $opcao, 'v', 'class=form-control');
+                        echo '</div>';
+                        $opcao = array(
+                            'v' => 'Viagem',
+                            'f' => 'Fretamento',
+                            't' => 'Turismo',
+                            'e' => 'Escursão'
+                        );
+                        echo "<div class='col-md-2'>";
+                        echo form_label('Tipo: ');
+                        echo form_dropdown('tipo', $opcao, 'v', 'class=form-control');
+                        echo '</div>';
+                        $query = $this->db->get('tb_viagem');
+                        $viagem[] = '';
+                        foreach ($query->result() as $vig) {
+                            $viagem[$vig->id_viagem] = $vig->destino;
+                        }
+                        echo "<div class='col-md-4'>";
+                        echo form_label('Destino: ');
+                        echo form_dropdown('id_viagem', $viagem, 'v', 'class=form-control');
+                        echo '</div>';
+                        echo '</div>';//inicio de uma nova linha
+                        echo "<div class='row'>";
+                        echo "<div class='col-md-2'>";
+                        echo form_label('Data Saida: ');
+                        echo "<input type='text' name='data_saida' id='data_saida' class='form-control input-sm'>";
+                        echo '</div>';
+                        echo "<div class='col-md-2'>";
+                        echo form_label('Data Retorno: ');
+                        echo "<input type='text' name='data_retorno' id='data_retorno' class='form-control input-sm'>";
+                        echo '</div>';
+                        $this->db->where('status', 'A');
+                        $query = $this->db->get('tb_drivers');
+                        $opcao2[] = '';
+                        foreach ($query->result() as $driver) {
+                            $opcao2[$driver->id_drivers] = $driver->nome;
+                        }
+                        echo "<div class='col-md-4'>";
+                        echo form_label('Motorista: ');
+                        echo form_dropdown('id_motorista', $opcao2, 'v', 'class=form-control');
+                        echo '</div>';
+                        echo "<div class='col-md-2'>";
+                        echo form_label('Preço: ');
+                        echo "<input type='text' name='preco' id='preco' class='form-control input-sm'>";
+                        echo '</div>';
+                        echo "<div class='col-md-2'>";
+                        echo form_label('Preço Un.: ');
+                        echo "<input type='text' name='preco_un' id='preco_un' class='form-control input-sm'>";
+                        echo '</div>';
+                        echo '</div>';
+                        echo "<div class='row'>";
+                        echo "<div class='col-md-12'>";
+                        echo form_label('Observação: ');
+                        echo "<input type='text' name='observacao' id='observacao' class='form-control input-sm'>";
+                        echo "</div>";
+                        echo '</div>';
+
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-close"></i> Cancelar</button>
+                <button type="button" class="btn btn-success" onclick="acaoMotorista();"><i class="fa fa-check"></i> Salvar</button>
+            </div>
+        </div>
+    </div>
+</div>
--- application/views/vw_agenda.php
+++ application/views/vw_agenda.php
@@ -5,3 +5,116 @@
  * Date: 17/03/2017
  * Time: 22:43
  */
+?>
+<!DOCTYPE html>
+<html>
+<!-- HEAD -->
+<?php
+include_once "menu/l_head.php";
+$agenda  = "active";
+$listaAgenda = '';
+
+?>
+<!-- END HEAD -->
+<!-- LEFT MENU -->
+<?php include_once "menu/l_menue.php"; ?>
+
+<!-- END LEFT MENU -->
+<!-- INICIO CONTEUDO -->
+<div class="content-wrapper" id="conteudo">
+    <!-- CONTEUDO -->
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Agenda
+            <small>Listagem</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-map-bus"></i> Agenda</a></li>
+            <li class="active">Lista</li>
+        </ol>
+    </section>
+
+    <div class="alert alert-danger alert-dismissable" style="display: none;" id="alertCadastroFalha">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <i class="icon fa fa-ban"></i> Erro ao cadastrar agenda!
+    </div>
+
+    <div class="alert alert-success alert-dismissable" style="display: none;" id="alertCadastroSucesso">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <i class="icon fa fa-check"></i> Agenda cadastrado com sucesso!
+    </div>
+
+    <div class="alert alert-danger alert-dismissable" style="display: none;" id="alertEditaFalha">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <i class="icon fa fa-ban"></i> Erro ao editar agenda!
+    </div>
+
+    <div class="alert alert-success alert-dismissable" style="display: none;" id="alertEditaSucesso">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <i class="icon fa fa-check"></i> Agenda editado com sucesso!
+    </div>
+
+    <!-- Main content -->
+    <section class="content">
+        <!-- Main row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <?php
+                echo form_button([
+                    "class"       =>"btn bg-purple",
+                    "content"     =>"<i class='fa fa-plus'></i> Cadastrar Viagem",
+                    "data-toggle" =>"modal",
+                    "onclick"     => "$('#acao_cadastro').val(1);limpaAgenda();",
+                    "data-target" =>'#cadastrarAgenda']);
+                ?>
+                <div id="agendaLista">
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Código</th>
+                            <th>Ônibus</th>
+                            <th>Destino</th>
+                            <th>Saída</th>
+                            <th>Retorno</th>
+                            <th>Tipo</th>
+                            <th>Situação</th>
+                            <th>Ação</th>
+                        </tr>
+
+                            <tr>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
+
+<!-- Modal -->
+<?php include_once "modal/vw_agendaModal.php"; ?>
+<!-- FIM MODAL -->
+<!-- FIM CONTEUDO -->
+</div><!-- FIM CONTEUDO -->
+
+<!-- FOOTER -->
+<?php include_once "menu/l_footer.php"; ?>
+<!-- END FOOTER -->
+
+<!-- RIGHT MENU -->
+<?php include_once "menu/l_menud.php"; ?>
+<!-- END RIGHT -->
+
+<div class="control-sidebar-bg"></div>
+</div><!-- ./wrapper -->
+<!-- JAVASCRIPT -->
+<?php include_once "menu/l_js.php"; ?>
+<!-- END JAVASCRIPT -->
+
+</body>
+</html>
